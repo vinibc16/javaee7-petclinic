9dcd2e59fa513cf0cc41fec4ab7edba20fcba1b0
vinibc16 <vb.canteiro@gmail.com>
2016-06-18 20:38:56 -0300

    autocomplete funcionando

--- src/main/webapp/mapa.js
+++ src/main/webapp/mapa.js
@@ -2,6 +2,7 @@ var geocoder;
 var map;
 var marker;
 var latlng;
+var autocomplete;
 
 function initialize() {
     latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
@@ -10,16 +11,12 @@ function initialize() {
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
     map = new google.maps.Map(document.getElementById("mapa"), options);
-
     geocoder = new google.maps.Geocoder();
-
     marker = new google.maps.Marker({
         map: map,
         draggable: true,
     });
-
     marker.setPosition(latlng);
 }
 
@@ -35,9 +32,37 @@ function carregarNoMapa(address) {
     });
 }
 
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('findAddr:txtEndereco')),
+            {types: ['geocode']});
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
+
 $(document).ready(function () {
     initialize();
-    
+    initAutocomplete();
     google.maps.event.addListener(marker, 'drag', function () {
         geocoder.geocode({'latLng': marker.getPosition()}, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
